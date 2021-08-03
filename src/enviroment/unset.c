cb5e10fe88fc0f5a5e61c2970ebebce9c43b48c8
cerebus <cerebus@student.21-school.ru>
2021-08-03 15:19:51 +0300

    отправила unset в новый файл

---
+++ src/enviroment/unset.c
@@ -0,0 +1,16 @@
+//
+// Created by Cesar Erebus on 8/3/21.
+//
+#include "envList.h"
+#include "../minishell.h"
+
+void ft_unset(t_msh *msh, char **names)
+{
+	t_en_list *envTmp;
+	t_en_list *expTmp;
+
+	envTmp = msh->env_list;
+	expTmp = msh->export_list;
+	deleteInList(&envTmp, names);
+	deleteInList(&expTmp, names);
+}
